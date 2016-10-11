59e32ecbd9303f3dc7ccde488046754aab01b047
xujiankang <xujiankang.sh@superjia.com>
2016-10-11 11:45:09 +0800

    Initial commit

---
+++ foodie-utils/src/main/java/com/lami/App.java
@@ -0,0 +1,13 @@
+package com.lami;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ foodie-web/src/main/java/com/lami/foodie/App.java
@@ -0,0 +1,7 @@
+package com.lami.foodie;
+
+/**
+ * Created by xjk on 2016/10/11.
+ */
+public class App {
+}
